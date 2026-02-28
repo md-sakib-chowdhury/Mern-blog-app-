7e303b34f0e6a85840b197484cc0099b580dafdc
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-28 10:46:02 +0600

    2nd commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+
+// configenv
+dotenv.config();
+//rest object
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.get('/', (req, res) => {
+    res.status(200).send({
+        "message": "Node server"
+    })
+})
+
+// Port
+const PORT = process.env.PORT || 8080;
+//listen
+app.listen(PORT, () => {
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan.white
+    );
+});
