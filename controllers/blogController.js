2ee6c81daf8f45edb718d8cb2abb7006f4085e5b
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-01 12:49:43 +0600

    10 th commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -28,10 +28,57 @@ exports.getAllBlogsController = async (req, res) => {
 };
 
 //Create Blog
-exports.createBlogController = () => { };
+exports.createBlogController = async (req, res) => {
+    try {
+        const { title, description, image } = req.body;
+        //validation
+        if (!title || !description || !image) {
+            return res.status(400).send({
+                success: false,
+                message: "Please Provide All Fields",
+            });
+        }
+        const newBlog = new blogModel({ title, description, image });
+        await newBlog.save();
+        return res.status(201).send({
+            success: true,
+            message: "Blog Created!",
+            newBlog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "Error WHile Creting blog",
+            error,
+        });
+    }
+};
 
 //Update Blog
-exports.updateBlogController = () => { };
+exports.updateBlogController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, image } = req.body;
+        const blog = await blogModel.findByIdAndUpdate(
+            id,
+            { ...req.body },
+            { new: true }
+        );
+        return res.status(200).send({
+            success: true,
+            message: "Blog Updated!",
+            blog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "Error WHile Updating Blog",
+            error,
+        });
+    }
+};
 
 //SIngle Blog
 exports.getBlogByIdController = () => { };
