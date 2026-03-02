02adef5ca88d4ce5d503f29af981253b22a3d04f
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-02 10:49:07 +0600

    12th comit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,6 @@
-
+const mongoose = require("mongoose")
 const blogModel = require("../models/blogModel");
+const userModel = require("../models/userModel")
 
 //GET ALL BLOGS
 exports.getAllBlogsController = async (req, res) => {
@@ -30,15 +31,30 @@ exports.getAllBlogsController = async (req, res) => {
 //Create Blog
 exports.createBlogController = async (req, res) => {
     try {
-        const { title, description, image } = req.body;
+        const { title, description, image ,user } = req.body;
         //validation
-        if (!title || !description || !image) {
+        if (!title || !description || !image || !user) {
             return res.status(400).send({
                 success: false,
                 message: "Please Provide All Fields",
             });
         }
-        const newBlog = new blogModel({ title, description, image });
+        const exisitingUser = await userModel.findById(user)
+        //validaton
+        if (!exisitingUser) {
+            return res.status(404).send({
+                success: false,
+                message: 'unable to find user'
+            })
+        }
+
+        const newBlog = new blogModel({ title, description, image, user});
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newBlog.save({ session });
+        exisitingUser.blogs.push(newBlog);
+        await exisitingUser.save({ session });
+        await session.commitTransaction();
         await newBlog.save();
         return res.status(201).send({
             success: true,
