467c4ec86e6465ddd51014e019426d30d729024a
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-03 10:16:47 +0600

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div>
-      <h1>BLOG APP</h1>
+      <Header />
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, AppBar, Toolbar, Button, Typography } from "@mui/material";
+
+const Header = () => {
+    return (
+        <>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Typography variant="h4">My Blog APP</Typography>
+                    <Box display={"flex"} marginLeft="auto">
+                        <Button sx={{ margin: 1, color: "white" }}>Login</Button>
+                        <Button sx={{ margin: 1, color: "white" }}>Register</Button>
+                        <Button sx={{ margin: 1, color: "white" }}>Logout</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+};
+
+export default Header;
