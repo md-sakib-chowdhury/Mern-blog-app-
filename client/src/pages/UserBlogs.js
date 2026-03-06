5e541b2a170d7ea1c25adf3997170c12e0e0f9ce
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-06 10:40:12 +0600

    fix package.json

--- client/src/pages/UserBlogs.js
+++ client/src/pages/UserBlogs.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const UserBlogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  //get user blogs
+  const getUserBlogs = async () => {
+    try {
+      const id = localStorage.getItem("userId");
+      const { data } = await axios.get(`/api/v1/blog/user-blog/${id}`);
+      if (data?.success) {
+        setBlogs(data?.userBlog.blogs);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserBlogs();
+  }, []);
+  console.log(blogs);
   return (
     <div>
-        <h1>UserBlogs</h1>
+      <h1>Users blogs</h1>
+    
     </div>
-  )
-}
+  );
 
-export default UserBlogs
+};
+export default UserBlogs;
